f389cb283fc465cd1a087f1ae239cfb36ed1f7b9
ksunilt <55142193+ksunilt@users.noreply.github.com>
2023-01-19 14:48:25 +0530

    fyers_authentication.py

--- fyers_auth.py
+++ fyers_auth.py
@@ -1,142 +1,120 @@
-from fyers_api import fyersModel
-from fyers_api import accessToken
-import webbrowser
-import json
-import pdb
-import ast
+import os
+from urllib.parse import urlparse, parse_qs
+from fyers_api import fyersModel, accessToken
+from utility.telegram_utility import send_tele_msg
+from yaml.loader import SafeLoader
 
+import requests
+import sys
+import yaml
 
-def read_token_file():
-    """
-    Function to read the access token. 
-    :return: reads the access token and share it with the parent function. 
-    """
-    with open('/sk_bot/telegram_service/tokenf.txt', 'r') as f:
+config_main = '../trade-assist/config/config_main.yml'
+with open(config_main, 'r') as config_main:
+    cfg = yaml.load(config_main, Loader=SafeLoader)
+
+# below items to be added to config_main and should be picking it from there.
+username = cfg['auth']['username'] # fyers_id
+password = cfg['auth']['password']  # fyers_password
+pin = cfg['auth']['pin']  # your integer pin
+client_id = cfg['auth']['client_id']  # "L9NY****W-100" (Client_id here refers to APP_ID of the created app)
+secret_key = cfg['auth']['secret_key']  # app_secret key which you got after creating the app
+redirect_uri = cfg['auth']['redirect_uri']  # redircet_uri you entered while creating APP.
+
+app_id = client_id[:-4]  # '##########'
+auth_logs = cfg['auth']['auth_logs']
+
+# to read the token file.
+def read_t_file():
+    with open("../trade-assist/authentication/tokenf.txt", "r") as f:
         token = f.read()
     return token
 
-def write_token_file(access_token):
-    """
-    :param access_token: Access token from fyers. 
-    :return: gets the access token from fyers and write it in txt file. 
-    """
-    with open('/sk_bot/telegram_service/tokenf.txt', 'w') as f:
-        f.write(access_token).close()
-
-def generate_auth_token():
-    """
-    The function is to generate authorisation token from your fyers app.
-    You will be redirected to the fyers using browser where you have to put you login and paswd. 
-    :return: redirect you to the app url (redirect_uri that you have configured while creating the app in fyers broker.) to get the auth token.
-    :roadmap: 1. need to read from the config file.
-            2. need to fully automate by not using the internet browser.
-    """
-    client_id = '<CLIENT_ID_HERE>'
-    secret_key = '<SECRET_KEY_HERE>'
-    redirect_uri = '<REDIRECT_URI_HERE>'
-    appSession = accessToken.SessionModel(client_id=client_id, secret_key=secret_key, redirect_uri=redirect_uri, response_type='code', grant_type='authorization_code')
-    response = appSession.generate_authcode()
-    print(response)
-    webbrowser.open(response, new=1)
+# to write the token to the token file and will be valid till 12 PM
+def write_file(token):
+    with open("../trade-assist/authentication/tokenf.txt", "w") as f:
+        f.write(token)
 
 
-def generate_access_token():
-    """
-    The function uses the Authorisation token from fyers and get the access token and calls write_token_file function to write the token in txt file.
-    :return: get access token by calling write_token_file function.
-    :roadmap: 1. need to read from the config file. 
-            2. need more automation here to get the non url version. 
-    """
-    auth_code = '<AUTH_CODE_HERE>'
-    client_id = '<CLIENT_ID_HERE>'
-    secret_key = '<SECRET_KEY_HERE>'
-    redirect_uri = '<CREDIRECT_URI_HERE>'
-    appSession = accessToken.SessionModel(client_id=client_id, secret_key=secret_key, redirect_uri=redirect_uri, response_type='code', grant_type='authorization_code')
-    appSession.set_token(auth_code)
-    response = appSession.generate_token()
-    try:
-        access_token = response['access_token']
-        write_token_file(access_token)
-        print("Token file is updated with new access token...")
-    except Exception as error:
-        print(response['message'])
+def setup():
+    session = accessToken.SessionModel(
+        client_id=client_id,
+        secret_key=secret_key,
+        redirect_uri=redirect_uri,
+        response_type="code",
+        grant_type="authorization_code"
+    )
 
+    headers = {
+        "accept": "application/json",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+        "accept-language": "en-US,en;q=0.9",
+    }
 
-def fyers_connect():
-    """
-    Function to read the token from .txt file and connect to fyers app.
-    This function can be used to called by other parent functions to perform some action in fyers.
-    :return: connects to fyers api.
-    :roadmap: 1. need to read from config file.
-            2. need to have a validation for the app connection and send confirmation/failure message to the user via telegram. 
-    """
-    client_id = '<CLIENT_ID_HERE>'
-    token = read_token_file()
-    log_path = '/sk_bot/logs/fyers_auth_logs/'
-    fyers = fyersModel.FyersModel(client_id=client_id, token=token, log_path=log_path)
-    return fyers
+    s = requests.Session()
+    s.headers.update(headers)
 
+    data1 = f'{{"fy_id":"{username}","password":"{password}","app_id":"2","imei":"","recaptcha_token":""}}'
+    r1 = s.post("https://api.fyers.in/vagator/v1/login", data=data1)
+    # print("this is data1 ==>", data1)
+    # print("this is r1 ==>", r1)
+    assert r1.status_code == 200, f"Error in r1:\n {r1.json()}"
 
-def validate_login():
-    """
-    Connect to fyers api to validate the connection. 
-    :return: user profile.
-    :roadmap: 1. need to use config file. 
-            2. need to put a validation on the password last change value. 
-    """
-    fyers = fyers_connect()
-    profile = fyers.get_profile()
-    if profile.get('code') == 200:
-        print("Login validation is PASSED.\n")
-        data = profile['data']
+    request_key = r1.json()["request_key"]
+
+    data2 = f'{{"request_key":"{request_key}","identity_type":"pin","identifier":"{pin}","recaptcha_token":""}}'
+    r2 = s.post("https://api.fyers.in/vagator/v1/verify_pin", data=data2)
+    # print("this is data2 ==>", data2)
+    # print("this is r2 ==>", r2)
+    assert r2.status_code == 200, f"Error in r2:\n {r2.json()}"
+
+    headers = {"authorization": f"Bearer {r2.json()['data']['access_token']}", "content-type": "application/json; charset=UTF-8"}
+    data3 = f'{{"fyers_id":"{username}","app_id":"{app_id}","redirect_uri":"{redirect_uri}","appType":"100","code_challenge":"","state":"abcdefg","scope":"","nonce":"","response_type":"code","create_cookie":true}}'
+    r3 = s.post("https://api.fyers.in/api/v2/token", headers=headers, data=data3)
+
+    assert r3.status_code == 308, f"Error in r3:\n {r3.json()}"
+
+    parsed = urlparse(r3.json()["Url"])
+    auth_code = parse_qs(parsed.query)["auth_code"][0]
+    session.set_token(auth_code)
+    response = session.generate_token()
+    token = response["access_token"]
+    write_file(token)
+
+    message = "Got the access token!!!"
+    send_tele_msg(message=message)
+
+    fyers = fyersModel.FyersModel(client_id=client_id, token=token, log_path=auth_logs)
+    fyers_profile = fyers.get_profile()
+    if fyers_profile.get('code') == 200:
+        data = fyers_profile['data']
+        msg = ("Login validation is PASSED")
+        send_tele_msg(message=msg)
         for key, val in data.items():
             print(key, ":",val)
     else:
-        print("Login validation FAILED.")
+        message = "Login validation FAILED."
+        send_tele_msg(message=message)
 
 
-def read_input_file(filename, path = '/sk_bot/config/'):
-    """
-    Function is called by parent function to read the file from the path.  
-    :param filename: any txt file name that you wanted to read.   
-    :param path: the path where the file exist. 
-    :return: list datatype.  
-    """
-    filePath = path + filename
-    with open(filePath, 'r') as f:
-        fav_stock = f.read()
-        fav_stock_response = ast.literal_eval(fav_stock)
-        return fav_stock_response
+def fyers_login():
+    try:
+        token = read_t_file()
+    except FileNotFoundError:
+        setup()
+        sys.exit()
+    fyers = fyersModel.FyersModel(client_id=client_id, token=token, log_path=auth_logs)
+    response = fyers.get_profile()
+    # print("response is ====>", response)
+    if "error" in response["s"] or "error" in response["message"] or "expired" in response["message"]:
+        setup()
+        return response
+    else:
+        message = "You already have a access token!"
+        send_tele_msg(message=message)
+        passwd_expiry = response['data']['pwd_to_expire']
+        if passwd_expiry < 5:
+            print(f"Please change the password. Will be expired in {passwd_expiry} days")
+            return response
 
-def get_quotes():
-    """
-    Function to get the quotes for the stock listed in the file from fyers.
-    :return: quote price.
-    """
-    data = read_input_file('fav_stocks.txt')
-    fyers = fyers_connect()
-    quote = fyers.quotes(data)
-    print(quote)
-    ltp = quote['d'][0]['v']['lp']
-    symbol = quote['d'][0]['n']
-    print(ltp)
-    print(symbol)
-    for k, v in quote.items():
-        while isinstance(v, dict):
-            myprint(v)
-        else:
-            print("{0} : {1}".format(k, v))
-
-            
-# print(generate_auth_token.__doc__)    # using doc string for the function. 
-# generate_auth_token()
-# generate_access_token()
-# validate_login()
-# print(read_input_file('fav_stocks.txt'))
-# get_quotes() #python dict
-
-# fyers_connect()
-
-# fav_stock = '/Users/sunilkumar/ksunilt/trading_bot/sk_bot/config/fav_stocks.txt'
-
-
+if __name__ == "__main__":
+    fyers_login()
